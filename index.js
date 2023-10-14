bf0b108e8263b5979898c47ae2b0ddb1c621af06
Mohammad Al Issa <mohammed.alissa627@gmail.com>
2023-10-14 19:27:01 +0300

    completed

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  let indexOfi = 0;
+  for (i of array){
+    indexOfi+=1;
+    let indexOfii = 0;
+    for (ii of array){
+      indexOfii+=1;
+      if(indexOfi!==indexOfii){
+      if (i+ii===target){
+        // console.log(i,ii,target)
+        return true
+      }
+    }
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+
+  O(n²)
 */
 
 /* 
-  Add your pseudocode here
+  iterate over the array of numbers
+        iterate over the array of numbers
+            check if the sum of two iterated items equals target
+                return true
+            else
+                return false    
+
 */
 
 /*
   Add written explanation of your solution here
+  a function that make a for loop of array then it makes another for loop to check the current loop with other loop item then return true and if condition is never met then it return false. 
 */
 
 // You can run `node index.js` to view these console logs
@@ -23,7 +46,7 @@ if (require.main === module) {
   console.log("");
 
   console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+  console.log("=>", hasTargetSum([2, 2, 3, 3], 4));
 
   console.log("");
 
